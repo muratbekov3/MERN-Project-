36f3c09bd5cadf95a538fdbb0abcf2fcf7e7041c
muratbekov3 <akylbekmuratbekuulu@gmail.com>
2022-05-05 06:14:56 +0200

    styles

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 import cors from "cors"
 import postRoutes from "./routes/posts.js"
 
-
-const app = express();
+ 
+const app = express()
 
 app.use('/posts', postRoutes)
 
