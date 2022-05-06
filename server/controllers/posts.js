680733e4cd942463ccb1fcefa2098fd0774be4fc
muratbekov3 <akylbekmuratbekuulu@gmail.com>
2022-05-06 12:04:30 +0200

    Patch update existing post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,14 +1,22 @@
+
+
+
+
+import mongoose from "mongoose"
 import PostMessage from "../models/postMessage.js"
+
+
+
 
 export const  getPosts = async (req,res) => {
     try {
-      const postMessages = PostMessage.find()   
-      
-      console.log(postMessages)
-
+      const postMessages = await PostMessage.find()   
+       console.log(postMessages)
       res.status(200).json(postMessages)
     } catch (error) {
+      // res.send('Нет соединении')
       res.status(404).json({message: error.message})
+      console.log('не нашло')
     }
 }
 
@@ -24,3 +32,20 @@ export const createPost = async (req, res) => {
         res.status(409).json({message: error.message})
     }
 }
+
+
+export const updatePost = async (req,res) => {
+  const {id: _id} = req.params
+  const post = req.body
+
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with this id')
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id,post, {new:true} )
+
+  res.json(updatedPost)
+}
+
+
+
+
+
--- server/index.js
+++ server/index.js
@@ -15,11 +15,11 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = "mongodb+srv://akylbek:101191@cluster0.c9k6x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://akylbek:101191@cluster0.1efiw.mongodb.net/myFirstDatabase"
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
-    .catch((error) => console.log(error.message) )
+    .catch((error) => console.log(`${error} база данных не соеденено`) )
 
 
